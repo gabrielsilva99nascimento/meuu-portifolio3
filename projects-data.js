0e161bfe3cb70bfcfe24d098424175566fc002bf
gabrielsilva99nascimento <gabrielsilvagns99@gmail.com>
2025-04-22 22:17:50 -0300

    update iimages

--- projects-data.js
+++ projects-data.js
@@ -18,8 +18,25 @@ const projects = [
         github: "https://github.com/gabrielsilva99nascimento/site-da-hamburgueria",
         demo: "#",
         images: [
-            "assets/images/background-hero.jpg"
+            "assets/images/burguerdev1.jpg",
+            "assets/images/burguerdev2.jpg",
+            "assets/images/burguerdev3.jpg",
+            "assets/images/burguerdev4.jpg"
         ]
     },
-   
+    {
+        id: 2,
+        title: "PRODUZAFIMLS",
+        description: "LANDING PAGE de uma produtora audiovisual onde o usuario pode ver o portifolio e entrar em contato com a produtora",
+        technologies: ["JavaScript", "HTML", "css3"],
+        github: "https://github.com/gabrielsilva99nascimento/PRODUZAFIMLS.git",
+        demo: "#",
+        images: [
+            "assets/images/produza1.jpg",
+            "assets/images/produza2.jpg",
+            "assets/images/produza3.jpg",
+            "assets/images/produza4.jpg",
+            "assets/images/produza5.jpg" 
+        ]
+    },
 ];
